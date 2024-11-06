c42afb96d43797b8f799f0432e1c8aa6e55b5a40
Erik Forkin <eforkin@indeed.com>
2024-11-06 11:39:17 -0600

    Update test file

--- src/__tests__/unit/lib/us-grid-emissions.test.ts
+++ src/__tests__/unit/lib/us-grid-emissions.test.ts
@@ -1,4 +1,30 @@
-// import {USGridEmissionsPlugin} from '../../../lib';
+import {USGridEmissionsPlugin} from '../../../lib';
 
 describe('lib/us-grid-emissions: ', () => {
+    const originalProcessEnv = process.env;
+    const spy = jest.spyOn(global.console, 'warn');
+    const parametersMetadata = {
+        inputs: {},
+        outputs: {},
+    };
+
+    beforeAll(() => {
+        process.env.EIA_API_KEY = 'mock-key';
+    });
+
+    afterAll(() => {
+        process.env = originalProcessEnv;
+        spy.mockReset();
+    });
+
+    describe('USGridEmissionsPlugin(): ', () => {
+        it('has metadata field.', () => {
+            const plugin = USGridEmissionsPlugin({}, parametersMetadata, {});
+
+            expect.assertions(3);
+            expect(plugin).toHaveProperty('metadata');
+            expect(plugin).toHaveProperty('execute');
+            expect(typeof plugin.execute).toBe('function');
+        });
+    });
 });
